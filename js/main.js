b79bd718bb77abd5a6e1d727b5d4e060d5b49ddd
Ico Lizhen <icolizhen@gmail.com>
2022-01-01 12:51:02 +0000

    fix issues from intermediate ev

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-"use strict";
+'use strict';
 
-const selectValue = document.querySelector(".js_selectValue");
-const playBtn = document.querySelector(".js_playBtn");
-const textResult = document.querySelector(".js_textResult");
-const counterJugador = document.querySelector(".js_counterJugador");
-const counterOrdenador = document.querySelector(".js_counterOrdenador");
+const selectValue = document.querySelector('.js_selectValue');
+const playBtn = document.querySelector('.js_playBtn');
+const textResult = document.querySelector('.js_textResult');
+const counterJugador = document.querySelector('.js_counterJugador');
+const counterOrdenador = document.querySelector('.js_counterOrdenador');
+
+let scoreJugador = 0;
+let scoreOrdenador = 0;
+let moves = 0;
+
+// Issue: make a function that replaces '.innerHTML' by using a parameter
+// function paintInnerHTML(element, content) {
+//   element.innerHTML = content;
+// }
+// paintInnerHTML(textResult, 'Hello');
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
@@ -13,55 +23,66 @@ function getRandomNumber(max) {
 function changeNumIntoValue() {
   let computerOption = getRandomNumber(9);
   if (computerOption < 4) {
-    computerOption = "piedra";
-  } else if (computerOption >= 4 && computerOption <= 6) {
-    computerOption = "papel";
+    computerOption = 'piedra';
+  } else if (computerOption > 3 && computerOption < 7) {
+    computerOption = 'papel';
   } else {
-    computerOption = "tijera";
+    computerOption = 'tijera';
   }
   return computerOption;
-}
-
-function updateCounter() {
-  let counterJugadorNum = parseInt(counterJugador.innerHTML);
-  let counterOrdenadorNum = parseInt(counterOrdenador.innerHTML);
-
-  if (counterJugadorNum < 10 && counterOrdenadorNum < 10) {
-    if (textResult.innerHTML === "¡Has ganado!") {
-      counterJugador.innerHTML = counterJugadorNum + 1;
-    } else if (textResult.innerHTML === "¡Has perdido!") {
-      counterOrdenador.innerHTML = counterOrdenadorNum + 1;
-    }
-  } else {
-    counterJugador.innerHTML = 0;
-    counterOrdenador.innerHTML = 0;
-    selectValue.value = "option-choose";
-    textResult.innerHTML = "¡Vamos a jugar!";
-  }
 }
 
 function compareUserOption() {
   const myOption = selectValue.value;
   const computerOption = changeNumIntoValue();
+  // console.log(`Player chose ${myOption}`);
+  // console.log(`Computer chose ${computerOption}`);
 
-  if (myOption === computerOption) {
-    textResult.innerHTML = "Empate";
-  } else if (myOption === "piedra" && computerOption === "papel") {
-    textResult.innerHTML = "¡Has perdido!";
-  } else if (myOption === "piedra" && computerOption === "tijera") {
-    textResult.innerHTML = "¡Has ganado!";
-  } else if (myOption === "papel" && computerOption === "piedra") {
-    textResult.innerHTML = "¡Has ganado!";
-  } else if (myOption === "papel" && computerOption === "tijera") {
-    textResult.innerHTML = "¡Has perdido!";
-  } else if (myOption === "tijera" && computerOption === "papel") {
-    textResult.innerHTML = "¡Has ganado!";
-  } else if (myOption === "tijera" && computerOption === "piedra") {
-    textResult.innerHTML = "¡Has perdido!";
+  if (scoreJugador < 10 && scoreOrdenador < 10) {
+    if (myOption === computerOption) {
+      textResult.innerHTML = 'Empate';
+    } else if (myOption === 'piedra' && computerOption === 'papel') {
+      textResult.innerHTML = '¡Has perdido!';
+      scoreOrdenador++;
+    } else if (myOption === 'piedra' && computerOption === 'tijera') {
+      textResult.innerHTML = '¡Has ganado!';
+      scoreJugador++;
+    } else if (myOption === 'papel' && computerOption === 'piedra') {
+      textResult.innerHTML = '¡Has ganado!';
+      scoreJugador++;
+    } else if (myOption === 'papel' && computerOption === 'tijera') {
+      textResult.innerHTML = '¡Has perdido!';
+      scoreOrdenador++;
+    } else if (myOption === 'tijera' && computerOption === 'papel') {
+      textResult.innerHTML = '¡Has ganado!';
+      scoreJugador++;
+    } else if (myOption === 'tijera' && computerOption === 'piedra') {
+      textResult.innerHTML = '¡Has perdido!';
+      scoreOrdenador++;
+    } else {
+      textResult.innerHTML = `Por favor, selecciona una opción para empezar a jugar`;
+    }
+    counterJugador.innerHTML = `Jugador: ${scoreJugador}`;
+    counterOrdenador.innerHTML = `Ordenador: ${scoreOrdenador}`;
   } else {
+    restartGame();
+  }
+}
+
+function restartGame() {
+  if (scoreJugador > scoreOrdenador) {
+    textResult.innerHTML = 'Has ganado la partida';
+  } else if (scoreOrdenador > scoreJugador) {
+    textResult.innerHTML = 'Has perdido la partida';
+  } else {
+    textResult.innerHTML = 'Habéis empatado';
+  }
+  counterJugador.innerHTML = `Jugador: 0`;
+  counterOrdenador.innerHTML = `Ordenador: 0`;
+  selectValue.value = 'option-choose';
+  if (selectValue.value === 'option-choose') {
     textResult.innerHTML = `Por favor, selecciona una opción para empezar a jugar`;
   }
-  updateCounter();
 }
 
 function handleClickBtn() {
@@ -69,4 +90,4 @@ function handleClickBtn() {
   compareUserOption();
 }
 
-playBtn.addEventListener("click", handleClickBtn);
+playBtn.addEventListener('click', handleClickBtn);
